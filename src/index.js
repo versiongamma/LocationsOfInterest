8b910af819431396541a71690d3a4f3fb088bb53
Matt <37131904+versiongamma@users.noreply.github.com>
2021-08-20 21:38:40 +1200

    update formatting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
+import { Container, Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
+import { format } from 'date-format-parse';
+
 const App = () => {
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch('https://placesofinterest.azurewebsites.net/')
+      .then(res => res.json())
+      .then(res => {
+        res.sort((a, b) => new Date(b.added) - new Date(a.added));
+        setData(res);
+      });
+  }, []);
+
   return (
-    <h1>
-      HELLO THERE
-    </h1>
+    <Container maxWidth='xl'>
+      {data !== undefined ? (
+      <Table stickyHeader>
+        <TableHead>
+          <TableRow>
+            <TableCell component='th'>Name</TableCell>
+            <TableCell component='th'>Address</TableCell>
+            <TableCell component='th'>Date</TableCell>
+            <TableCell component='th'>Time</TableCell>
+            <TableCell component='th'>Date Added</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map(row => (
+            <TableRow>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.address}</TableCell>
+              <TableCell>
+                {
+                  format(new Date(row.date), 'dddd D') + 
+                  (format(new Date(row.date), 'D') === '1' ? 'st' : 
+                  format(new Date(row.date), 'D') === '2' ? 'nd' :
+                  format(new Date(row.date), 'D') === '21' ? 'st' :
+                  format(new Date(row.date), 'D') === '22' ? 'nd' :
+                  format(new Date(row.date), 'D') === '31' ? 'st' : 'th')
+                }
+              </TableCell>
+              <TableCell>{row.time}</TableCell>
+              <TableCell>
+                {
+                  format(new Date(row.added), 'D') + 
+                  (format(new Date(row.added), 'D') === '1' ? 'st' : 
+                  format(new Date(row.added), 'D') === '2' ? 'nd' :
+                  format(new Date(row.added), 'D') === '21' ? 'st' :
+                  format(new Date(row.added), 'D') === '22' ? 'nd' :
+                  format(new Date(row.added), 'D') === '31' ? 'st' : 'th') + format(new Date(row.added), ' MMMM')
+                }
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      ) : null }
+    </Container>
   )
 }
 
